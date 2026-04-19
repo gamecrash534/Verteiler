4a87a19c3fa085e0d7fdf0b60de7904f118faaaf
gamecrash534 <73080127+gamecrash534@users.noreply.github.com>
2026-04-19 16:40:37 +0200

    small optimization in app.js, remove uneccessary veriables from styles.css

--- src/main/resources/web/static/js/app.js
+++ src/main/resources/web/static/js/app.js
@@ -1,15 +1,12 @@
 function toggleTheme() {
-    let theme = window.localStorage.getItem("theme");
+    const theme = window.localStorage.getItem("theme");
     setTheme(theme === "light" ? "dark" : "light");
 }
 
 function setTheme(theme) {
-    if (theme == null) theme = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)') ? "dark" : "light";
+    if (!theme) theme = window.matchMedia?.('(prefers-color-scheme: dark)').matches ? "dark" : "light";
     window.localStorage.setItem("theme", theme);
-
-    if (theme === "dark") document.body.setAttribute("data-theme", "dark");
-    else document.body.setAttribute("data-theme", "light");
-
+    document.body.dataset.theme = theme;
     document.getElementById("theme-toggle").textContent = `[theme:${theme}]`;
 }
 
